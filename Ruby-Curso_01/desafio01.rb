a79c88e414e476ad81aafd5da1ed043f50e0fc98
Lucas Bernardelli dos Passos <PassosL@outlook.com>
2023-03-14 00:05:33 -0300

    Add files via upload

---
+++ Ruby-Curso_01/desafio01.rb
@@ -0,0 +1,7 @@
+print "Bem-vindo - Digite seu nome: "
+nome = gets.chomp
+print "Muito bem - Digite seu sobrenome: "
+sobrenome = gets.chomp
+print "Excelente - Digite sua idade: "
+idade = gets.chomp
+puts "Olá #{nome} #{sobrenome}! Seja bem-vindo! Sua idade é #{idade} anos."
