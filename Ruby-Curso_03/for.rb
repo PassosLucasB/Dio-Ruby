1877ef69c4dc0f953c4d7b23c4910c6baa9a2a09
Lucas Bernardelli dos Passos <PassosL@outlook.com>
2023-03-15 23:49:51 -0300

    Add files via upload

---
+++ Ruby-Curso_03/for.rb
@@ -0,0 +1,9 @@
+#leia uma lisata (array) de elementos - linguagens de programação
+#primeiro - criar a lista
+linguagens = ['Ruby', 'Go', 'C++']
+
+#apareça na tela (puts) essa lista
+#laço For
+for linguagem in linguagens
+    puts linguagem
+end
