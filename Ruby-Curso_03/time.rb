4057d94ba0dc07ef99f28d27275663892fa20b3f
Lucas Bernardelli dos Passos <PassosL@outlook.com>
2023-03-16 00:38:40 -0300

    Add files via upload

---
+++ Ruby-Curso_03/time.rb
@@ -0,0 +1,26 @@
+#exemplo 1 puts
+#6.times {puts "Puts"}
+
+#exemplo 1 print
+#6.times {print "Print "}
+
+
+#exemplo 2 puts
+#4.times do
+#    puts "puts"
+#end
+
+#exemplo 2 print
+#4.times do
+#    print "print"
+#end
+
+#exemplo 3 contador puts
+#20.times  do |contador|
+#    puts "#{contador}"
+#end
+
+#exemplo 3 contador print
+20.times  do |contador|
+    print "#{contador}"
+end
