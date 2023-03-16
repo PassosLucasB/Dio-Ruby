99a091e81895d0801ad8a24c84c9c2f0f8bebfb1
Lucas Bernardelli dos Passos <PassosL@outlook.com>
2023-03-15 23:53:52 -0300

    Add files via upload

--- Ruby-Curso_03/for.rb
+++ Ruby-Curso_03/for.rb
@@ -1,9 +1,11 @@
 #leia uma lisata (array) de elementos - linguagens de programação
 #primeiro - criar a lista
 linguagens = ['Ruby', 'Go', 'C++']
-
+count = 1
 #apareça na tela (puts) essa lista
 #laço For
 for linguagem in linguagens
     puts linguagem
+    puts count
+    count = count + 1
 end
