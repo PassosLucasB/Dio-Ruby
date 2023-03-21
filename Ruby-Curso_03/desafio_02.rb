041238c128e9e4f36f81e060e99dc3b40b05b02d
Lucas Bernardelli dos Passos <PassosL@outlook.com>
2023-03-20 21:18:01 -0300

    Add files via upload

--- Ruby-Curso_03/desafio_02.rb
+++ Ruby-Curso_03/desafio_02.rb
@@ -2,11 +2,16 @@
 # "gets" : lê UMA linha com dado(s) de entrada (inputs) do usuário;
 # "puts": imprime um texto de saída (output), pulando linha.
 # ".to_i": Converte para int
-
-PI = 3.14159
-raio = gets().to_f
-
 #TODO:  Crie as condições necessárias para o programa calcular o volume
 
-VOLUME = (4/3.0)*PI*(raio*raio*raio)
-puts "VOLUME = #{VOLUME.round(3)} "
+PI = 3.14159
+
+raio = gets().to_f
+
+formula = (4/3.0) * PI * (raio ** 3)
+
+  
+
+#puts "VOLUME = #{formula.round(3)}"
+
+puts ("VOLUME = %.3f" % formula)
