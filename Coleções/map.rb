c77ecfcd253405208df3e44724efeea96c03dcdc
Lucas Bernardelli dos Passos <PassosL@outlook.com>
2023-03-27 23:44:13 -0300

    Add files via upload

---
+++ Coleções/map.rb
@@ -0,0 +1,23 @@
+#cria um arry (lista) baseado em um outro aray (lista) existente
+
+
+# #.map não altera o conteúdo do array original
+# =.map do ||
+# *5
+# end
+
+# puts "\n Array Original"
+# puts " #{}"
+
+# puts "\n Novo Array"
+# puts " #{}"
+
+# # puts "\n Executando .map! multiplicando casa item por 2"
+# # #.map! força que o conteúdo da array original seja alterado
+# # array.map! d |a|
+# # a*2
+# # end
+
+# # puts "\n Array Original"
+# # puts " #{array}"
+# puts ''
