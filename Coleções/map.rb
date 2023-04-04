9f10be124bf4104aab3ca5d6e5fbdea35a158cbc
Lucas Bernardelli dos Passos <PassosL@outlook.com>
2023-04-03 23:52:03 -0300

    Add files via upload

--- Coleções/map.rb
+++ Coleções/map.rb
@@ -1,10 +1,11 @@
 #cria um arry (lista) baseado em um outro aray (lista) existente
 
+numeros = [2, 3, 4, 5]
 
-# #.map não altera o conteúdo do array original
-# =.map do ||
-# *5
-# end
+#.map não altera o conteúdo do array original
+ novo_numeros = numeros.map do |x|
+    *5
+ end
 
 # puts "\n Array Original"
 # puts " #{}"
