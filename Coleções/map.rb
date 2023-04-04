35fa6bbb526e9359dd6372ac5d8f08466f5254e8
Lucas Bernardelli dos Passos <PassosL@outlook.com>
2023-04-04 00:31:51 -0300

    Add files via upload

--- Coleções/map.rb
+++ Coleções/map.rb
@@ -3,22 +3,22 @@
 numeros = [2, 3, 4, 5]
 
 #.map não altera o conteúdo do array original
- novo_numeros = numeros.map do |x|
-    *5
- end
+# novo_numeros = numeros.map do |x|
+#    x * 5
+#    end
 
-# puts "\n Array Original"
-# puts " #{}"
+# puts "\n Array Original"  #\ -> pular uma linha
+# puts " #{numeros}"
 
 # puts "\n Novo Array"
-# puts " #{}"
+# puts " #{novo_numeros}"
 
-# # puts "\n Executando .map! multiplicando casa item por 2"
-# # #.map! força que o conteúdo da array original seja alterado
-# # array.map! d |a|
-# # a*2
-# # end
 
-# # puts "\n Array Original"
-# # puts " #{array}"
-# puts ''
+#.map! força que o conteúdo da array original seja alterado
+ numeros.map! do |x|
+    x * 5
+ end
+
+puts "\n Array Original"
+puts " #{numeros}"
+puts ''
