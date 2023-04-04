e0cd2633b3f748fe0b7d56a9f43504ca59d5214e
Lucas Bernardelli dos Passos <PassosL@outlook.com>
2023-04-04 01:35:07 -0300

    Add files via upload

--- Coleções/select.rb
+++ Coleções/select.rb
@@ -4,17 +4,30 @@
 
 #Primeiro vamos usar SELECT com ARRAY
 #Criar nosso array
-
+#numeros = [-5,-4,-3,-2,-1,0,1,2,3,4,5,6,7,8,9,10]
 
 #(quem são os selecionados) = arrayoriginal.select do |percorre o array|
 # (variável de percorre o array) (condição de existência ) (o que ele deve executar)
 #end
+#selecionados = numeros.select do |n|
+#    n > 0
+#end
+#puts (os selecionados)
+#puts selecionados
 
-#puts (os elementos)
+
 
 #agora vamos usar o SELECT em um HASH
 
 # (criar um hash)= {0 => 'zero', 1 => 'um', 2 => 'dois', 3 => 'tres'}
-
+numeros = {0 => 'zero', 1 => 'um', 2 => 'dois', 3 => 'tres'}
 # puts 'Selecionando keys com valor maior que 0'
+puts 'Vamos selecionar Chaves (Keys) com valores maiores que 0'
 # (chave escolhida) = hash.select do |key, value|
+#        key (condição de existência)
+#    end
+selecionados_chaves = numeros.select do |key, value|
+    key  > 0
+end
+    #puts (chaves escolhidas)
+puts selecionados_chaves
