20ec36189e4e2dc6aa71d6643dc08ce2e002ea10
Lucas Bernardelli dos Passos <PassosL@outlook.com>
2023-04-05 23:19:31 -0300

    Add files via upload

---
+++ Desafio_potencia.rb
@@ -0,0 +1,13 @@
+potencia = []
+puts "Digite um número: "
+a = gets.chomp.to_i
+puts "Digite um número: "
+b = gets.chomp.to_i
+puts "Digite um número: "
+c = gets.chomp.to_i
+x = a**3
+y = b**3
+z = c**3
+potencia.push(x, y, z)
+puts "#{potencia}"
+
