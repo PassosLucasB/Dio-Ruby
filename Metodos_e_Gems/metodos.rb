a316fb56a3bb50931a7baac093e117ff4c5eb821
Lucas Bernardelli dos Passos <PassosL@outlook.com>
2023-04-08 01:22:45 -0300

    Add files via upload

---
+++ Metodos_e_Gems/metodos.rb
@@ -0,0 +1,11 @@
+def oi
+    puts 'Oi dev!'
+end
+oi
+
+def nome (nome, sobrenome)
+    puts "Oi #{nome} #{sobrenome}, você é um Dev em Ruby!"
+end
+nome = 'Lucas'
+sobrenome = 'Passos'
+nome(nome, sobrenome)
