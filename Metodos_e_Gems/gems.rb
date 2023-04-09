bfad6d71a8e3c30850a4979704127a0f7323391e
Lucas Bernardelli dos Passos <PassosL@outlook.com>
2023-04-08 22:44:20 -0300

    Add files via upload

---
+++ Metodos_e_Gems/gems.rb
@@ -0,0 +1,17 @@
+#a biblioteca qe usaremos nessa aula será a os
+
+#1. instalar a biblioteca (gem)
+
+# Escrever nosso código
+#iremos codar um programa que retorna algumas das configurações do nosso computador
+#para isso:
+
+#.2 definir um método
+#esse método vai analisar qual o sistema operacional do nosso computador
+#dentro desse método, nosso código precisa analisar SE(IF) nosso sistema operacional é
+#X, SENÃO (ELSEIF) Y, SENÃO(ELSEIF) Z, SENÃO (ELSE) outra coisa
+#não esqueça de finalizar tanto o IF quando o DEF
+
+#3. No final de tudo queremos que o programa imprima qual o sist. operacional
+# e o número de bits e as cores do seu PC
+#"Meu PC é ........ bits, possui ......cores e o sist. operacional é .........."
